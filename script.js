afba7035df0db187f9cb7869867f023be3c47ac1
Ruhi Tamanna Alam <ruhialamsithy@gmail.com>
2018-10-08 03:31:03 +0000

    task 1 to 5

--- script.js
+++ script.js
@@ -19,3 +19,65 @@ console.log(sumArray([2, 4, 5, 6, 7]));
     console.log(checkEmail("jhon@smith.com"));
     console.log(checkEmail("jhon@smith.com"));
     
+    var library = [ 
+   {
+       author: 'Bill Gates',
+       title: 'The Road Ahead',
+       readingStatus: true
+   },
+   {
+       author: 'Steve Jobs',
+       title: 'Walter Isaacson',
+       readingStatus: true
+   },
+   {
+       author: 'Suzanne Collins',
+       title:  'Mockingjay: The Final Book of The Hunger Games', 
+       readingStatus: false
+   }];
+function getReadingStatus(index){
+    var book = library[index]
+    return book.readingStatus;
+}
+console.log(getReadingStatus(1));
+console.log(getReadingStatus(0));
+console.log(getReadingStatus(2));
+
+var cart = [ 
+   {
+       name: 'Shoes',
+       price: 560,
+       quantity: 4
+   },
+   {
+       name: 'Regular Tees',
+       price: 455.50,
+       quantity: 6
+   },
+   {
+       name: 'Socks',
+       price: 65.99,
+       quantity: 2
+   }];
+function addItem(newItem){
+    cart.push(newItem);
+    console.log(cart);
+}
+var newItem={name: 'watch',
+price:64,
+quantity:1,
+};
+addItem(newItem);
+
+function sortcart(keyName){
+    
+}
+
+
+
+function findByName(name){
+
+
+
+
+}
